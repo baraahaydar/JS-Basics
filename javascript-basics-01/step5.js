9165b5a82a488bcf6cc69f30659f83f62613ee13
baraa-haydar <baraa97.bh@gmail.com>
2021-03-09 18:45:48 +0200

    step 5

---
+++ javascript-basics-01/step5.js
@@ -0,0 +1,6 @@
+function msg() {
+    var fn=document.getElementById('first_number').Value;
+    var sn=document.getElementById('second_number').Value;
+    var fmsg = fn * sn ;
+    alert(fn + " * " + sn + " = " + fmsg);
+}
