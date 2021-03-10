4e0ce81dd91b816f7a7ed061b288e02a46502f6e
baraa-haydar <baraa97.bh@gmail.com>
2021-03-10 13:56:33 +0200

    step 6 basics 2

---
+++ javascript-basics-02/step 6/step6.js
@@ -0,0 +1,51 @@
+const img1 = document.querySelector("#image1");
+const img2 = document.querySelector("#image2");
+const img3 = document.querySelector("#image3");
+const img4 = document.querySelector("#image4");
+const img5 = document.querySelector("#image5");
+
+const changeImg = (id) =>{
+    if(id == "img1"){
+    img1.src = "images/image1_2.jpg";
+}
+else if(id == "img2"){
+    img2.src = "images/image2_2.jpg";
+}
+else if(id == "img3"){
+    img3.src = "images/image3_2.jpg";
+}
+else if(id == "img4"){
+    img4.src = "images/image4_2.jpg";
+}
+else if(id == "img5")
+    img5.src = "images/image5_2.jpg";
+};
+
+img1.addEventListener("mouseover", ()=>{changeImg("img1")});
+img2.addEventListener("mouseover", ()=>{changeImg("img2")});
+img3.addEventListener("mouseover", ()=>{changeImg("img3")});
+img4.addEventListener("mouseover", ()=>{changeImg("img4")});
+img5.addEventListener("mouseover", ()=>{changeImg("img5")});
+
+const RchangeImg = (i) => {
+    if(i == "img1"){
+        img1.src = "images/image1.jpg";
+    }
+    else if(i == "img2"){
+        img2.src = "images/image2.jpg";
+    }
+    else if(i == "img3"){
+        img3.src = "images/image3.jpg";
+    }
+    else if(i == "img4"){
+        img4.src = "images/image4.jpg";
+    }
+    else if(i == "img5")
+        img5.src = "images/image5.jpg";
+    };
+
+img1.addEventListener("mouseout", () => { RchangeImg("img1") });
+img2.addEventListener("mouseout", () => { RchangeImg("img2") });
+img3.addEventListener("mouseout", () => { RchangeImg("img3") });
+img4.addEventListener("mouseout", () => { RchangeImg("img4") });
+img5.addEventListener("mouseout", () => { RchangeImg("img5") });
