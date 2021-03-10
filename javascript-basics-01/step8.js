c41994d7c572053544327fec4593f7711e51c946
baraa-haydar <baraa97.bh@gmail.com>
2021-03-10 12:27:31 +0200

    step 1 basics 2

---
+++ javascript-basics-01/step8.js
@@ -0,0 +1,8 @@
+function msg() {
+    var age = document.getElementById('age').value;
+    if (age < 18){
+        alert("you are under 18");
+    }
+    else
+    alert("you are over 18");
+}
---
+++ javascript-basics-02/step 1/step1.js
@@ -0,0 +1,4 @@
+function pic(){
+    const image1 = document.querySelector("#image1")
+    image1.src= "images/image1_2.jpg";
+}
