dfc5705a9e0095f04522eac96939ce652ceb78d1
baraa-haydar <baraa97.bh@gmail.com>
2021-03-10 16:07:34 +0200

    step 3 basics 3

---
+++ javascript-basics-03/step 3/step3.js
@@ -0,0 +1,11 @@
+const t = document.getElementById("text");
+
+function re(){
+    t.style.color = "red";
+}
+function gr(){
+    t.style.color = "green";
+}
+function bl(){
+    t.style.color = " blue";
+}
